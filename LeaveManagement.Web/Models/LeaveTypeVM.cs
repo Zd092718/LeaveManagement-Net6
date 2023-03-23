0ed3c56da6b79e60da605406e1f496c12e60d587
Zack Dowd <zdowd2796@gmail.com>
2023-03-23 09:27:38 -0600

    modified crud screens and added databases and swal

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name ="Default Number of Days")]
+        [Required]
+        public int DefaultDays { get; set; }
+    }
+}
